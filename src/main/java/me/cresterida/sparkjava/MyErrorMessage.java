ab65fb09626ae79d06fc4cede905300b9cd2a312
kiquetal <kiquetal@gmail.com>
2017-02-12 23:27:52 -0300

    removing clases

--- src/main/java/me/cresterida/sparkjava/MyErrorMessage.java
+++ src/main/java/me/cresterida/sparkjava/MyErrorMessage.java
@@ -12,7 +12,7 @@ import java.util.Map;
  *
  * @author kiquetal
  */
-public class MyErrorMessage implements MyMessage{
+public final class MyErrorMessage implements MyMessage{
     
     private String message;    
     private Map<String,String> error=new HashMap<>();
